8037de4d1be8b8e677433685b2bdc2770901d01b
krishna-s11 <krishnasaxena69@gmail.com>
2025-05-06 06:00:15 +0530

    demo fix

--- src/components/Home/TradingTable/TradingTable.jsx
+++ src/components/Home/TradingTable/TradingTable.jsx
@@ -10,10 +10,18 @@ const TradingTable = () => {
             <tr>
                 <th>Asset</th>
                 <th>Type</th>
-                <th>Enthy Price</th>
+                <th>Entry Price</th>
                 <th>Confidence %</th>
                 <th>Status</th>
                 <th>P&L</th>
+            </tr>
+            <tr>
+              <td>Bitcoin</td>
+              <td>Payment</td>
+              <td>$94708</td>
+              <td>92%</td>
+              <td style={{color: "#01B574"}}>Live</td>
+              <td style={{color: "#E31A1A"}}>-0.003%</td>
             </tr>
         </table>
     </div>
