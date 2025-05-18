e37cb145425d137e794dee7af0ffc05a62af7e46
krishna-s11 <krishnasaxena69@gmail.com>
2025-05-19 02:21:51 +0530

    ngrok proxy

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -41,7 +41,7 @@ const Login = () => {
       e.preventDefault();
       setLoading(true);
       try {
-        const response = await axios.post('https://13.53.142.82/auth/login', {
+        const response = await axios.post('https://1a76-13-53-142-82.ngrok-free.app/auth/login', {
           email: formData.email,
           password: formData.password
         });
